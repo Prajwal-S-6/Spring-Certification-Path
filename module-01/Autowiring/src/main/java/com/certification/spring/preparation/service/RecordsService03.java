81298faa9ff73d6f654c76ff56ff3f398993fea9
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-05-21 23:45:17 +0530

    @Autowired on constructor with multiple constructors

---
+++ module-01/Autowiring/src/main/java/com/certification/spring/preparation/service/RecordsService03.java
@@ -0,0 +1,40 @@
+package com.certification.spring.preparation.service;
+
+import com.certification.spring.preparation.bls.DbRecordsProcessor;
+import com.certification.spring.preparation.non.beans.RecordsHash;
+import com.certification.spring.preparation.non.beans.RecordsUtil;
+import com.certification.spring.preparation.non.beans.RecordsValidator;
+import com.certification.spring.preparation.reader.DbRecordsReader;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class RecordsService03 {
+
+    @Autowired
+    private RecordsService03(DbRecordsReader recordsReader, DbRecordsProcessor recordsProcessor, Optional<RecordsUtil> recordsUtil, @Nullable RecordsHash recordsHash, @Autowired(required = false) RecordsValidator recordsValidator) {
+        System.out.println(
+                getClass().getSimpleName() + " recordsReader = " + recordsReader + "\n" +
+                        getClass().getSimpleName() + " recordsProcessor = " + recordsProcessor + "\n" +
+                        getClass().getSimpleName() + " recordsUtil = " + recordsUtil + "\n" +
+                        getClass().getSimpleName() + " recordsHash = " + recordsHash + "\n" +
+                        getClass().getSimpleName() + " recordsValidator = " + recordsValidator + "\n"
+        );
+    }
+
+    // @Autowired
+    RecordsService03(DbRecordsReader recordsReader, DbRecordsProcessor recordsProcessor) {
+        System.out.println(
+                getClass().getSimpleName() + " recordsReader = " + recordsReader + "\n" +
+                        getClass().getSimpleName() + " recordsProcessor = " + recordsProcessor + "\n"
+        );
+
+    }
+
+    public RecordsService03() {
+        System.out.println(getClass().getSimpleName() + "----------------------- constructor");
+    }
+}
