923058a12fc054e599cabb3deec751961ed57e3e
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-05-26 12:29:13 +0530

    @Profile - custom profile annotation

--- module-01/Profiles-1/src/main/java/com/certification/spring/preparation/definition/custom/profile/ApplicationConfiguration.java
+++
@@ -1,8 +0,0 @@
-package com.certification.spring.preparation.definition.custom.profile;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class ApplicationConfiguration {
-}
--- module-01/Profiles-1/src/main/java/com/certification/spring/preparation/definition/custom/profile/annotations/FileStoreProfile.java
+++
@@ -1,7 +0,0 @@
-package com.certification.spring.preparation.definition.custom.profile.annotations;
-
-import org.springframework.context.annotation.Profile;
-
-@Profile("file")
-public @interface FileStoreProfile {
-}
