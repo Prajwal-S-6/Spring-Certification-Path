84aa3038da36f8547a13a5d75898efbec9b5d5e7
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-05-26 18:14:49 +0530

    @Profile activation

--- module-01/Profiles-1/src/main/java/com/certification/spring/preparation/activation/vm/property/Runner.java
+++ module-01/Profiles-1/src/main/java/com/certification/spring/preparation/activation/vm/property/Runner.java
@@ -1,6 +1,6 @@
 package com.certification.spring.preparation.activation.vm.property;
 
-import com.certification.spring.preparation.activation.programmatically.dao.FinancialDataDao;
+import com.certification.spring.preparation.activation.vm.property.dao.FinancialDataDao;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Runner {
@@ -8,7 +8,7 @@ public class Runner {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfiguration.class);
         context.registerShutdownHook();
-
+        // by setting vm run options: -Dspring.profiles.active=database
         FinancialDataDao financialDataDao = context.getBean(FinancialDataDao.class);
         System.out.println(financialDataDao.getClass().getSimpleName());
     }
