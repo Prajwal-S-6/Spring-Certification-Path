0030fb3934a6d4aa87c082cad75278d3f3690b0c
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-05-26 17:12:04 +0530

    @Profile

--- module-01/Profiles-1/src/main/java/com/certification/spring/preparation/definition/configuration/level/Runner.java
+++ module-01/Profiles-1/src/main/java/com/certification/spring/preparation/definition/configuration/level/Runner.java
@@ -10,7 +10,7 @@ public class Runner {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
         context.registerShutdownHook();
 
-        context.getEnvironment().setActiveProfiles("file");
+        context.getEnvironment().setActiveProfiles("database");
         context.register(ApplicationConfiguration.class);
         context.refresh();
 
