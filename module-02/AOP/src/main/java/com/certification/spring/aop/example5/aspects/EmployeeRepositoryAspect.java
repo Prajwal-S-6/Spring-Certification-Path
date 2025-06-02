9d9fbbb8bb732101d6575e65ce11091e1bc694ab
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-06-02 22:14:51 +0530

    Advices type - @After

--- module-02/AOP/src/main/java/com/certification/spring/aop/example5/aspects/EmployeeRepositoryAspect.java
+++ module-02/AOP/src/main/java/com/certification/spring/aop/example5/aspects/EmployeeRepositoryAspect.java
@@ -1,5 +1,7 @@
 package com.certification.spring.aop.example5.aspects;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -9,7 +11,12 @@ import org.springframework.stereotype.Component;
 public class EmployeeRepositoryAspect {
 
     @Before("execution(* com.certification.spring.aop.example5.repository.*.*(..))")
-    public void beforeAllMethodExecution() {
-        System.out.println("Before - All method execution");
+    public void beforeAllMethodExecution(JoinPoint joinPoint) {
+        System.out.println("Before - " + joinPoint.getSignature());
+    }
+
+    @After("execution(* com.certification.spring.aop.example5.repository.*.*(..))")
+    public void afterAllMethodExecution(JoinPoint joinPoint) {
+        System.out.println("After - " + joinPoint.getSignature());
     }
 }
