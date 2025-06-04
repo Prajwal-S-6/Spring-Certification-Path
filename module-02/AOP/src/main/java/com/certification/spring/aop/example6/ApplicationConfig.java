07196cce43ce293f6a6ab8115a49a58521355f5b
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-06-04 22:54:39 +0530

    Enabling @Aspect

--- module-02/AOP/src/main/java/com/certification/spring/aop/example6/ApplicationConfig.java
+++ module-02/AOP/src/main/java/com/certification/spring/aop/example6/ApplicationConfig.java
@@ -1,9 +1,22 @@
 package com.certification.spring.aop.example6;
 
+import com.certification.spring.aop.example6.aspects.HelloBeanAspect;
+import com.certification.spring.aop.example6.beans.HelloBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @ComponentScan
 @EnableAspectJAutoProxy
 public class ApplicationConfig {
+
+    @Bean
+    public HelloBeanAspect helloBeanAspect() {
+        return new HelloBeanAspect();
+    }
+
+    @Bean
+    HelloBean helloBean() {
+        return new HelloBean();
+    }
 }
--- module-02/AOP/src/main/java/com/certification/spring/aop/example6/beans/HelloBean.java
+++ module-02/AOP/src/main/java/com/certification/spring/aop/example6/beans/HelloBean.java
@@ -2,7 +2,7 @@ package com.certification.spring.aop.example6.beans;
 
 import org.springframework.stereotype.Component;
 
-@Component
+// @Component
 public class HelloBean {
     public void sayHello() {
         System.out.println("Hello from Spring bean");
