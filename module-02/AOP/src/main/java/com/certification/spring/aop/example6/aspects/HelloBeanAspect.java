4781202a1fab5b161531d58fda201005983b0907
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-06-04 22:53:03 +0530

    Enabling @Aspect

---
+++ module-02/AOP/src/main/java/com/certification/spring/aop/example6/aspects/HelloBeanAspect.java
@@ -0,0 +1,19 @@
+package com.certification.spring.aop.example6.aspects;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class HelloBeanAspect {
+
+    @Around("within(com.certification.spring.aop.example6.beans.*)")
+    public void withinPackage(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        System.out.println("Before - Around" + proceedingJoinPoint.getSignature());
+        proceedingJoinPoint.proceed();
+        System.out.println("After - Around" + proceedingJoinPoint.getSignature());
+    }
+}
