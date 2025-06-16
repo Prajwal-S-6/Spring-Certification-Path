f615488f23d016ac99742656a8eb7e710ba5d7f1
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-06-16 08:20:18 +0530

    Finder methods naming convention

--- module-03/SpringData/src/main/java/com/spring/data/example27/dao/EmployeeDao.java
+++ module-03/SpringData/src/main/java/com/spring/data/example27/dao/EmployeeDao.java
@@ -3,9 +3,18 @@ package com.spring.data.example27.dao;
 import com.spring.data.example27.ds.Employee;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Date;
 import java.util.List;
 
 public interface EmployeeDao extends CrudRepository<Employee, Integer> {
 
     List<Employee> findTop3ByOrderBySalaryDesc();
+
+    Employee findByFirstNameAndLastName(String firstName, String lastName);
+
+    Employee findByHireDateBetween(Date minData, Date maxDate);
+
+    List<Employee> findByOrderByHireDateDesc();
+
+
 }
