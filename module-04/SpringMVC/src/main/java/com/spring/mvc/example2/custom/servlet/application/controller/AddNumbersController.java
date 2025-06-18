d60e8695dc0d82269fcdb4ef25de231f6524f076
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-06-18 10:05:07 +0530

    Dispatcher servlet

--- module-04/SpringMVC/src/main/java/com/spring/mvc/example2/custom/servlet/application/controller/AddNumbersController.java
+++ module-04/SpringMVC/src/main/java/com/spring/mvc/example2/custom/servlet/application/controller/AddNumbersController.java
@@ -4,15 +4,27 @@ import com.spring.mvc.example2.custom.servlet.application.view.AddNumbersView;
 import com.spring.mvc.example2.custom.servlet.framework.controller.CustomController;
 import com.spring.mvc.example2.custom.servlet.framework.controller.IController;
 import com.spring.mvc.example2.custom.servlet.framework.ds.ModelAndView;
+import com.spring.mvc.example2.custom.servlet.framework.model.Model;
 import com.spring.mvc.example2.custom.servlet.framework.model.SimpleModel;
 import jakarta.servlet.http.HttpServletRequest;
+
+import static java.lang.String.valueOf;
 
 @CustomController("/mvc")
 public class AddNumbersController implements IController {
     @Override
     public ModelAndView handleRequest(HttpServletRequest request) {
+        Model model = new SimpleModel();
+
+        int numberA = Integer.parseInt(request.getParameter("numberA"));
+        int numberB = Integer.parseInt(request.getParameter("numberB"));
+
+        model.set("numberA", valueOf(numberA));
+        model.set("numberB", valueOf(numberB));
+        model.set("result", valueOf(numberA + numberB));
+
         return new ModelAndView(
-                new SimpleModel(),
+                model,
                 new AddNumbersView()
         );
     }
--- module-04/SpringMVC/src/main/java/com/spring/mvc/example2/custom/servlet/application/controller/SquareNumberController.java
+++ module-04/SpringMVC/src/main/java/com/spring/mvc/example2/custom/servlet/application/controller/SquareNumberController.java
@@ -1,16 +1,28 @@
 package com.spring.mvc.example2.custom.servlet.application.controller;
 
 import com.spring.mvc.example2.custom.servlet.application.view.SquareNumberView;
+import com.spring.mvc.example2.custom.servlet.framework.controller.CustomController;
 import com.spring.mvc.example2.custom.servlet.framework.controller.IController;
 import com.spring.mvc.example2.custom.servlet.framework.ds.ModelAndView;
+import com.spring.mvc.example2.custom.servlet.framework.model.Model;
 import com.spring.mvc.example2.custom.servlet.framework.model.SimpleModel;
 import jakarta.servlet.http.HttpServletRequest;
 
+import static java.lang.String.valueOf;
+
+@CustomController("/mvc")
 public class SquareNumberController implements IController {
     @Override
     public ModelAndView handleRequest(HttpServletRequest request) {
+        Model model =new SimpleModel();
+
+        int number = Integer.parseInt(request.getParameter("number"));
+
+        model.set("number", valueOf(number));
+        model.set("numberSquare", valueOf(number * number));
+
         return new ModelAndView(
-                new SimpleModel(),
+                model,
                 new SquareNumberView()
         );
     }
