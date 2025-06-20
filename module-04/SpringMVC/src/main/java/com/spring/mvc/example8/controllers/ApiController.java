907fbac419143e6c8467c54fa7e91746c4fdbff3
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-06-20 09:57:10 +0530

    @PathVariable

--- module-04/SpringMVC/src/main/java/com/spring/mvc/example8/controllers/ApiController.java
+++ module-04/SpringMVC/src/main/java/com/spring/mvc/example8/controllers/ApiController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Optional;
+
 @Controller
 public class ApiController {
 
@@ -19,4 +21,24 @@ public class ApiController {
     public String actionB(@PathVariable(name = "city") String city) {
         return String.format("PathVariable city: %s", city);
     }
+
+    @GetMapping("/actionC/country/{countries}/city/{city}")
+    @ResponseBody
+    public String actionC(@PathVariable(value = "countries") String country, @PathVariable(name = "city") String city) {
+        return String.format("PathVariable. country: %s, city: %s",country, city);
+    }
+
+    @GetMapping({"/actionD/country/{countries}/city/{city}", "/actionD/country/{countries}"})
+    @ResponseBody
+    public String actionD(@PathVariable(value = "countries") String country, @PathVariable(name = "city", required = false) String city) {
+        return String.format("PathVariable. country: %s, city: %s",country, city);
+    }
+
+    @GetMapping({"/actionE/country/{countries}/city/{city}", "/actionE/country/{countries}"})
+    @ResponseBody
+    public String actionE(@PathVariable(value = "countries") String country, @PathVariable(name = "city") Optional<String> city) {
+        return String.format("PathVariable. country: %s, city: %s",country, city.orElse("NY"));
+    }
+
+
 }
