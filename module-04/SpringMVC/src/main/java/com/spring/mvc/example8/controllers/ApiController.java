2226e4f7cfdc46df0ad8d820fc05c415ebfdf64d
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-06-20 10:04:11 +0530

    @PathVariable

--- module-04/SpringMVC/src/main/java/com/spring/mvc/example8/controllers/ApiController.java
+++ module-04/SpringMVC/src/main/java/com/spring/mvc/example8/controllers/ApiController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Controller
@@ -31,14 +33,26 @@ public class ApiController {
     @GetMapping({"/actionD/country/{countries}/city/{city}", "/actionD/country/{countries}"})
     @ResponseBody
     public String actionD(@PathVariable(value = "countries") String country, @PathVariable(name = "city", required = false) String city) {
-        return String.format("PathVariable. country: %s, city: %s",country, city);
+        return String.format("PathVariable city not required. country: %s, city: %s",country, city);
     }
 
     @GetMapping({"/actionE/country/{countries}/city/{city}", "/actionE/country/{countries}"})
     @ResponseBody
     public String actionE(@PathVariable(value = "countries") String country, @PathVariable(name = "city") Optional<String> city) {
-        return String.format("PathVariable. country: %s, city: %s",country, city.orElse("NY"));
+        return String.format("PathVariable optional value. country: %s, city: %s",country, city.orElse("NY"));
     }
 
+
+    @GetMapping("/actionF/country/{countries}/city/{city}/zip/{zipCode}")
+    @ResponseBody
+    public String actionF(@PathVariable Map<String, String> pathMap) {
+        return String.format("PathVariable map. country: %s, city: %s, zip: %s", pathMap.get("countries"), pathMap.get("city"), pathMap.get("zipCode"));
+    }
+
+    @GetMapping("/actionG/countries/{countries}")
+    @ResponseBody
+    public String actionG(@PathVariable("countries") List<String> countries) {
+        return String.format("PathVariable List: %s", String.join(",", countries));
+    }
 
 }
