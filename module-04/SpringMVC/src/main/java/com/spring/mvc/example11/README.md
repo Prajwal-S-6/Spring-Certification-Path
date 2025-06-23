2acff1e34587665968934a300ed8304e14c54379
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-06-23 09:04:01 +0530

    ReturnType: DeferredResult<>

--- module-04/SpringMVC/src/main/java/com/spring/mvc/example11/README.md
+++ module-04/SpringMVC/src/main/java/com/spring/mvc/example11/README.md
@@ -10,3 +10,4 @@ ReturnType of @Controller methods
 * ModelAndView - to specify view and model attributes, also can specify response status. View can be specified as logical view name or view instance. Model can be model object or Map
 * void - methods returning void can be handled using ServletResponse or OutputStream or @ResponseStatus. If none of this is handling the method, then he view name is auto resolved using **RequestToViewNameTranslator**
             in rest controller it means response with no body.
+* DeferredResult<> - allows to specify return value in controller asynchronously from different thread or some event callback
---
+++ module-04/SpringMVC/src/main/java/com/spring/mvc/example11/controllers/Type11DeferredResult.java
@@ -0,0 +1,40 @@
+package com.spring.mvc.example11.controllers;
+
+import com.spring.mvc.example11.ds.Person;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.async.DeferredResult;
+
+import static com.spring.mvc.example11.utils.SleepUtil.sleep;
+
+@Controller
+public class Type11DeferredResult {
+
+    private Logger logger = LoggerFactory.getLogger(Type11DeferredResult.class);
+
+    @Autowired
+    private TaskExecutor taskExecutor;
+
+    @GetMapping("/actionJ")
+    @ResponseBody
+    public DeferredResult<Person> actionJ() {
+        DeferredResult<Person> personDeferredResult = new DeferredResult<>();
+        taskExecutor.execute(() -> {
+            logger.info("Sleeping for some seconds");
+            try {
+                sleep();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            logger.info("Finished sleeping, giving results");
+            personDeferredResult.setResult(new Person("Praj", "S"));
+
+        });
+        return personDeferredResult;
+    }
+}
