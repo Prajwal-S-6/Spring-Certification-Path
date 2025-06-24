bed542b93bead5f540effee80f65a403c013f0bd
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-06-24 09:29:14 +0530

    REST Http methods

--- module-05/SpringREST/src/main/java/com/spring/rest/example1/controllers/CustomerApiController.java
+++ module-05/SpringREST/src/main/java/com/spring/rest/example1/controllers/CustomerApiController.java
@@ -2,11 +2,17 @@ package com.spring.rest.example1.controllers;
 
 import com.spring.rest.example1.dao.CustomerDao;
 import com.spring.rest.example1.ds.Customer;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.net.URI;
 import java.util.List;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @RestController
 public class CustomerApiController {
@@ -18,4 +24,21 @@ public class CustomerApiController {
     public List<Customer> getCustomers() {
         return customerDao.findAll();
     }
+
+    @GetMapping("/customers/{id}")
+    public Customer getCustomer(@PathVariable("id") Integer id) {
+        return customerDao.findById(id).orElseThrow(() -> new ResponseStatusException(NOT_FOUND));
+    }
+
+    @PostMapping("/customers")
+    public ResponseEntity<Customer> addCustomer(@RequestBody @Valid Customer customer, BindingResult bindingResult) {
+        if(bindingResult.hasErrors()) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        Customer savedCustomer = customerDao.save(customer);
+        return ResponseEntity.created(URI.create("/customers" + savedCustomer.getId())).body(savedCustomer);
+    }
+
+
 }
