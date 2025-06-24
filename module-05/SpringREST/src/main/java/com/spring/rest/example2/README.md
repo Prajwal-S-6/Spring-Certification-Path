54611dee1ee11f5fabc9aad5f9a1c09cabf20bdc
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-06-24 09:46:34 +0530

    Web Resources

---
+++ module-05/SpringREST/src/main/java/com/spring/rest/example2/README.md
@@ -0,0 +1,9 @@
+**Resources**
+
+* Resource is an information made available via URI.
+* It can be text, document, video, image, text file.
+* REST uses different forms to represent resources
+* Client can specify the format in which the resources they want
+* Eg: JSON, XML, Text, HTML
+* These resource can be single resource or collection of resources
+* With REST services we can access/modify these resources using - HTTP GET, POST, UT, DELETE
--- module-05/SpringREST/src/main/resources/data.sql
+++ module-05/SpringREST/src/main/resources/data.sql
@@ -1,5 +1,19 @@
-insert into customer(id, code, first_name, last_name, address) values(1, 'CC', 'Caitlin', 'Chen', '190 Buttonwood Dr. Olympia, WA 98512');
-insert into customer(id, code, first_name, last_name, address) values(2, 'KT', 'Kamila', 'Terry', '73 South John Street Redondo Beach, CA 90278');
-insert into customer(id, code, first_name, last_name, address) values(3, 'EH', 'Eve', 'Harrell', '525 Main St. Austin, MN 55912');
-insert into customer(id, code, first_name, last_name, address) values(4, 'AH', 'Adalyn', 'Hooper', '82 Ashley Drive Coraopolis, PA 15108');
-insert into customer(id, code, first_name, last_name, address) values(5, 'CF', 'Chase', 'Freeman', '9357 South Newbridge Road Glenside, PA 19038');
+--insert into customer(id, code, first_name, last_name, address) values(1, 'CC', 'Caitlin', 'Chen', '190 Buttonwood Dr. Olympia, WA 98512');
+--insert into customer(id, code, first_name, last_name, address) values(2, 'KT', 'Kamila', 'Terry', '73 South John Street Redondo Beach, CA 90278');
+--insert into customer(id, code, first_name, last_name, address) values(3, 'EH', 'Eve', 'Harrell', '525 Main St. Austin, MN 55912');
+--insert into customer(id, code, first_name, last_name, address) values(4, 'AH', 'Adalyn', 'Hooper', '82 Ashley Drive Coraopolis, PA 15108');
+--insert into customer(id, code, first_name, last_name, address) values(5, 'CF', 'Chase', 'Freeman', '9357 South Newbridge Road Glenside, PA 19038');
+
+
+insert into customer(id, code, first_name, last_name) values(1, 'CC', 'Caitlin', 'Chen');
+insert into address(id, address_name, street_number, street_name, apt_number, city, state, zip_code, customer_id) values(1, 'Home Address', 3992, 'Buffalo Creek Road', 12, 'Nashville', 'TN', '37209', 1);
+insert into address(id, address_name, street_number, street_name, apt_number, city, state, zip_code, customer_id) values(2, 'Business Address', 2337, 'Bastin Drive', 14, 'Northampton', 'PA', '18067', 1);
+
+insert into customer(id, code, first_name, last_name) values(2, 'KT', 'Kamila', 'Terry');
+insert into address(id, address_name, street_number, street_name, apt_number, city, state, zip_code, customer_id) values(3, 'Home Address', 4363, 'Valley Drive', 8, 'Philadelphia', 'PA', '19108', 2);
+insert into address(id, address_name, street_number, street_name, apt_number, city, state, zip_code, customer_id) values(4, 'Business Address', 3224, 'Seneca Drive', 16, 'Portland', 'OR', '97217', 2);
+
+insert into customer(id, code, first_name, last_name) values(3, 'EH', 'Eve', 'Harrell');
+insert into address(id, address_name, street_number, street_name, apt_number, city, state, zip_code, customer_id) values(5, 'Home Address', 1755, 'Cemetery Street', 15, 'Salinas', 'CA', '93901', 3);
+insert into address(id, address_name, street_number, street_name, apt_number, city, state, zip_code, customer_id) values(6, 'Business Address', 1758, 'Ritter Avenue', 5, 'Roseville', 'MI', '48066', 3);
+
