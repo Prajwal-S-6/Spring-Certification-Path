45afeec5ffe54e0d7c9a0c8ab6a8444c9185fc58
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-06-26 09:15:51 +0530

    @Controller with @ResponseBody

--- module-05/SpringREST/src/main/java/com/spring/rest/example9/controllers/ArticlesController.java
+++ module-05/SpringREST/src/main/java/com/spring/rest/example9/controllers/ArticlesController.java
@@ -1,11 +1,17 @@
 package com.spring.rest.example9.controllers;
 
+
 import com.spring.rest.example9.dao.ArticlesDao;
+import com.spring.rest.example9.ds.Article;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 public class ArticlesController {
@@ -16,5 +22,17 @@ public class ArticlesController {
     public ModelAndView articles() {
        return new ModelAndView("index9",
                "articles", articlesDao.findAll());
+    }
+
+
+    @GetMapping("/articlesResponseBody")
+    @ResponseBody
+    public List<Article> articleListResponseBody() {
+        return articlesDao.findAll();
+    }
+
+    @GetMapping("/articlesNoResponseBody")
+    public List<Article> articleListNoResponseBody() {
+        return articlesDao.findAll();
     }
 }
