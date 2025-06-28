6862d2f256491a768097321b93c1cc95bdf3fc59
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-06-28 07:33:04 +0530

    spring-webmvc on classpath

---
+++ module-05/SpringREST/SpringREST-01/src/main/java/com/spring/rest/ds/Customer.java
@@ -0,0 +1,42 @@
+package com.spring.rest.ds;
+
+
+
+public class Customer {
+    private String code;
+    private String firstName;
+    private String lastName;
+
+    public Customer() {
+    }
+
+    public Customer(String code, String firstName, String lastName) {
+        this.code = code;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
