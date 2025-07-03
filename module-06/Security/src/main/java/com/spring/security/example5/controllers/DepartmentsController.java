7a0404bd3cc43f70e819abf954ec5ec429f31bb5
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-07-03 09:31:09 +0530

    RequestMatcher patterns

--- module-06/Security/src/main/java/com/spring/security/example5/controllers/DepartmentsController.java
+++ module-06/Security/src/main/java/com/spring/security/example5/controllers/DepartmentsController.java
@@ -1,10 +1,10 @@
 package com.spring.security.example5.controllers;
 
-import com.spring.security.example3.dao.DepartmentDao;
-import com.spring.security.example3.ds.Department;
+import com.spring.security.example5.dao.DepartmentDao;
+import com.spring.security.example5.ds.Department;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,25 +13,27 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import static com.spring.security.example5.security.SecurityRoles.*;
+
 @Controller
 public class DepartmentsController {
     
     @Autowired
     private DepartmentDao departmentsDao;
 
-    @PreAuthorize("hasAnyAuthority('DEPARTMENTS_READ', 'DEPARTMENTS_PAG_VIEW') || hasRole('SUPER_ADMIN')")
+    @Secured(ROLE_PREFIX + DEPARTMENTS_READ)
     @GetMapping("/departments")
     public ModelAndView index() {
         return new ModelAndView("departments1", "departments", departmentsDao.findAll());
     }
 
-    @PreAuthorize("hasAuthority('DEPARTMENTS_CREATE') || hasRole('SUPER_ADMIN')")
+    @Secured(ROLE_PREFIX + DEPARTMENTS_CREATE)
     @GetMapping("/departments/create")
     public ModelAndView create() {
         return new ModelAndView("department-create1", "department", new Department());
     }
 
-    @PreAuthorize("hasAuthority('DEPARTMENTS_CREATE') || hasRole('SUPER_ADMIN')")
+    @Secured(ROLE_PREFIX + DEPARTMENTS_CREATE)
     @PostMapping("/departments/create")
     public String create(@ModelAttribute @Valid Department department, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
@@ -43,7 +45,7 @@ public class DepartmentsController {
         }
     }
 
-    @PreAuthorize("hasAuthority('DEPARTMENTS_DELETE') || hasRole('SUPER_ADMIN')")
+    @Secured(ROLE_PREFIX + DEPARTMENTS_DELETE)
     @GetMapping("/departments/delete/{id}")
     public String delete(@PathVariable Integer id) {
         departmentsDao.deleteById(id);
