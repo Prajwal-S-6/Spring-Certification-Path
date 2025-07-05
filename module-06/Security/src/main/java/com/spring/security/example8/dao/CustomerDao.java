ae0f590192667fb220a9b11894ddaa58226778d1
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-07-05 09:50:10 +0530

    prePostEnabled annotations

--- module-06/Security/src/main/java/com/spring/security/example8/dao/CustomerDao.java
+++ module-06/Security/src/main/java/com/spring/security/example8/dao/CustomerDao.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PostFilter;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.access.prepost.PreFilter;
 
 import java.util.Collections;
 import java.util.List;
@@ -12,14 +13,22 @@ import java.util.Optional;
 
 public interface CustomerDao extends JpaRepository<Customer, Integer> {
 
-    @PostFilter("hasRole('CUSTOMERS_ADMIN') || (hasRole('CUSTOMERS_QA') && filterObject.getFirstName().equals('TEST'))")
+    @PostFilter("hasRole('ROLE_CUSTOMERS_READ') || (hasRole('ROLE_CUSTOMERS_QA') && filterObject.getFirstName().equals('TEST'))")
     @Override
     List<Customer> findAll();
 
 
-    @PostAuthorize("hasRole('CUSTOMERS_ADMIN') || (hasRole('CUSTOMERS_QA') && returnObject.isPresent() && returnObject.getFirstName().equals('TEST'))")
+    @PostAuthorize("hasRole('ROLE_CUSTOMERS_READ') || (hasRole('CUSTOMERS_QA') && returnObject.isPresent() && returnObject.getFirstName().equals('TEST'))")
     @Override
     Optional<Customer> findById(Integer id);
 
 
+
+    @PreFilter("hasRole('ROLE_CUSTOMERS_CREATE') || (hasRole('CUSTOMERS_QA') && filterObject.getFirstName().equals('TEST'))")
+    @Override
+    <S extends Customer> List<S> saveAll(Iterable<S> entities);
+
+    @PreAuthorize("hasRole('ROLE_CUSTOMERS_DELETE') || (hasRole('CUSTOMERS_QA') && 'TEST'.equals(#entity.getFirstName()))")
+    @Override
+    void delete(Customer entity);
 }
