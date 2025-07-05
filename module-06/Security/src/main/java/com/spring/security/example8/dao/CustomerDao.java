f9ccd66b20da7acb92f52bb284fc9a5ca64e93fb
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-07-05 09:41:50 +0530

    prePostEnabled annotations

--- module-06/Security/src/main/java/com/spring/security/example8/dao/CustomerDao.java
+++ module-06/Security/src/main/java/com/spring/security/example8/dao/CustomerDao.java
@@ -2,8 +2,24 @@ package com.spring.security.example8.dao;
 
 import com.spring.security.example8.ds.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PostFilter;
+import org.springframework.security.access.prepost.PreAuthorize;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
 
 public interface CustomerDao extends JpaRepository<Customer, Integer> {
+
+    @PostFilter("hasRole('CUSTOMERS_ADMIN') || (hasRole('CUSTOMERS_QA') && filterObject.getFirstName().equals('TEST'))")
+    @Override
+    List<Customer> findAll();
+
+
+    @PostAuthorize("hasRole('CUSTOMERS_ADMIN') || (hasRole('CUSTOMERS_QA') && returnObject.isPresent() && returnObject.getFirstName().equals('TEST'))")
+    @Override
+    Optional<Customer> findById(Integer id);
 
 
 }
