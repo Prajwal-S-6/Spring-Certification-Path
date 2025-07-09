a33bb6d39108d90a2103e9ed4f55b3874eb9c6f8
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-07-09 19:55:36 +0530

    JWT

--- module-06/Security/src/main/java/com/spring/security/example10/Application.java
+++ module-06/Security/src/main/java/com/spring/security/example10/Application.java
@@ -9,18 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class Application implements CommandLineRunner {
-
-    @Autowired
-    private UsersDao usersDao;
-
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
-    @Override
-    public void run(String... args) throws Exception {
-        usersDao.save(new Users(1, "admin", passwordEncoder.encode("password1")));
-    }
+public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
