c4246149f23c68dc1b6fdb042e948a5b69cafa56
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-07-09 19:59:08 +0530

    JWT

--- module-06/Security/src/main/java/com/spring/security/example10/WebSecurityConfig.java
+++ module-06/Security/src/main/java/com/spring/security/example10/WebSecurityConfig.java
@@ -1,4 +1,24 @@
 package com.spring.security.example10;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.authorizeHttpRequests(auth -> auth
+                .requestMatchers("/login").permitAll()
+                .anyRequest().authenticated())
+                .csrf(AbstractHttpConfigurer::disable)
+                .headers(httpSecurityHeadersConfigurer -> httpSecurityHeadersConfigurer.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
+                .build();
+    }
 }
---
+++ module-06/Security/src/main/java/com/spring/security/example10/controllers/IndexController.java
@@ -0,0 +1,7 @@
+package com.spring.security.example10.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+}
