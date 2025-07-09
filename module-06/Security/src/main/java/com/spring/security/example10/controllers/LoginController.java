4761a9eac16d72beda9ada44eea542a639dff30b
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-07-09 20:17:05 +0530

    JWT

--- module-06/Security/src/main/java/com/spring/security/example10/controllers/LoginController.java
+++ module-06/Security/src/main/java/com/spring/security/example10/controllers/LoginController.java
@@ -2,6 +2,10 @@ package com.spring.security.example10.controllers;
 
 import com.spring.security.example10.dto.LoginRequest;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +13,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class LoginController {
 
+    @Autowired
+    AuthenticationManager authenticationManager;
+
     @PostMapping("/login")
     public String login(@RequestBody @Valid LoginRequest loginRequest) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.username(), loginRequest.password()));
         return "jwt token";
+    }
+
+    @GetMapping("/test")
+    public String getTest() {
+        return "Success";
     }
 }
