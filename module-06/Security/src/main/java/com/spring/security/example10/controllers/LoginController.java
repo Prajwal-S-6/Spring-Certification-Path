be6734c486b83c9b8b2f89b6d2de71ce7ea47a23
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-07-09 20:31:33 +0530

    JWT

--- module-06/Security/src/main/java/com/spring/security/example10/controllers/LoginController.java
+++ module-06/Security/src/main/java/com/spring/security/example10/controllers/LoginController.java
@@ -1,6 +1,7 @@
 package com.spring.security.example10.controllers;
 
 import com.spring.security.example10.dto.LoginRequest;
+import com.spring.security.example10.utils.JwtUtils;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -16,12 +17,15 @@ public class LoginController {
     @Autowired
     AuthenticationManager authenticationManager;
 
+    @Autowired
+    JwtUtils jwtUtils;
+
     @PostMapping("/login")
     public String login(@RequestBody @Valid LoginRequest loginRequest) {
 
         try {
             authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.username(), loginRequest.password()));
-            return "jwt token";
+            return jwtUtils.generateToken(loginRequest.username());
         } catch (Exception e) {
             throw e;
         }
