51ad7804f8563ff53bcc4cee602c077052f96245
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-07-10 22:23:48 +0530

    JWT

---
+++ module-06/Security/src/main/java/com/spring/security/example10/JwtAuthenticationFilter.java
@@ -0,0 +1,32 @@
+package com.spring.security.example10;
+
+import com.spring.security.example10.utils.JwtUtils;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    @Autowired
+    JwtUtils jwtUtils;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authHeader = request.getHeader("Authorization");
+        String authToken = null;
+
+        if(authHeader != null) {
+            authToken = authHeader.substring(7);
+            jwtUtils.validateToken(authToken);
+
+        }
+
+    }
+}
