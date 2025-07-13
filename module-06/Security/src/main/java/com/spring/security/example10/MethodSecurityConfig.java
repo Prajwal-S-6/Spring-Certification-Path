9497a02eca6cdb15d9065e71b8aaa7aaea09160e
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-07-13 12:55:03 +0530

    MethodSecurity - Authorization

---
+++ module-06/Security/src/main/java/com/spring/security/example10/MethodSecurityConfig.java
@@ -0,0 +1,30 @@
+package com.spring.security.example10;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
+import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+
+@Configuration
+@EnableMethodSecurity
+public class MethodSecurityConfig {
+
+    @Bean
+    public RoleHierarchy roleHierarchy() {
+        return RoleHierarchyImpl.withDefaultRolePrefix()
+                .role("ADMIN").implies("READ")
+                .role("ADMIN").implies("WRITE")
+                .role("ADMIN").implies("DELETE")
+                .build();
+    }
+
+    @Bean
+    public MethodSecurityExpressionHandler methodSecurityExpressionHandler(RoleHierarchy roleHierarchy) {
+        DefaultMethodSecurityExpressionHandler defaultMethodSecurityExpressionHandler = new DefaultMethodSecurityExpressionHandler();
+        defaultMethodSecurityExpressionHandler.setRoleHierarchy(roleHierarchy);
+        return defaultMethodSecurityExpressionHandler;
+    }
+}
