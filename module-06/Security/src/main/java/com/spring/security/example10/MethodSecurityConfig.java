6179edce65e8b67c613cc4e2dd62ed984bee7d54
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-07-14 21:38:34 +0530

    MethodSecurity - Authorization

--- module-06/Security/src/main/java/com/spring/security/example10/MethodSecurityConfig.java
+++ module-06/Security/src/main/java/com/spring/security/example10/MethodSecurityConfig.java
@@ -2,6 +2,7 @@ package com.spring.security.example10;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.expression.SecurityExpressionRoot;
 import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
 import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
@@ -9,6 +10,7 @@ import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.core.GrantedAuthorityDefaults;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.web.access.expression.WebSecurityExpressionRoot;
 
 @Configuration
 @EnableMethodSecurity
@@ -23,16 +25,14 @@ public class MethodSecurityConfig {
                 .build();
     }
 
-//    @Bean
-//    public MethodSecurityExpressionHandler methodSecurityExpressionHandler(RoleHierarchy roleHierarchy) {
-//        DefaultMethodSecurityExpressionHandler defaultMethodSecurityExpressionHandler = new DefaultMethodSecurityExpressionHandler();
-//        defaultMethodSecurityExpressionHandler.setRoleHierarchy(roleHierarchy);
-//        defaultMethodSecurityExpressionHandler.setDefaultRolePrefix("MY_ROLE_");
-//        return defaultMethodSecurityExpressionHandler;
-//    }
+    @Bean
+    public MethodSecurityExpressionHandler methodSecurityExpressionHandler(RoleHierarchy roleHierarchy) {
+        return new CustomMethodSecurityExpressionHandler(roleHierarchy, "MY_ROLE_");
+    }
 
     @Bean
     public GrantedAuthorityDefaults grantedAuthorityDefaults() {
         return new GrantedAuthorityDefaults("MY_ROLE_");
     }
+
 }
