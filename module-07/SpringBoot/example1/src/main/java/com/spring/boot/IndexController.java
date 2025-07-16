4f3a499715fbe910cde1fdf038646aa59cf3af47
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-07-16 08:12:55 +0530

    SpringBoot

---
+++ module-07/SpringBoot/example1/src/main/java/com/spring/boot/IndexController.java
@@ -0,0 +1,32 @@
+package com.spring.boot;
+
+import com.spring.boot.dao.TodoItemDao;
+import com.spring.boot.ds.TodoItem;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @Autowired
+    private TodoItemDao todoItemDao;
+
+    @GetMapping("/")
+    public String items(Model model) {
+        model.addAttribute("todoItems", todoItemDao.findAll());
+        model.addAttribute("newTodoItem", new TodoItem());
+        return "index";
+    }
+
+    @PostMapping("/add")
+    public String addItem(@ModelAttribute("newTodoItem") TodoItem todoItem) {
+        todoItemDao.save(todoItem);
+
+        return "redirect:/";
+    }
+}
