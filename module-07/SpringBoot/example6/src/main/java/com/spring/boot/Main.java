72c8a1af409e22447caf7aefaf809de76b6c9d2f
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-07-17 07:42:37 +0530

    profile specific properties files

--- module-07/SpringBoot/example6/src/main/java/com/spring/boot/Main.java
+++ module-07/SpringBoot/example6/src/main/java/com/spring/boot/Main.java
@@ -1,7 +1,24 @@
 package com.spring.boot;
 
-public class Main {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication
+@EnableConfigurationProperties(ApplicationConfig.class)
+public class Main implements CommandLineRunner {
+
+    @Autowired
+    private ApplicationConfig config;
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        SpringApplication.run(Main.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println(config);
     }
 }
