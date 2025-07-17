6dde64ee388df9d8424b71f700b7369216570ad5
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-07-17 07:55:30 +0530

    Accessing properties: @Value , Also Using Environment object

--- module-07/SpringBoot/example7/src/main/java/com/spring/boot/Main.java
+++ module-07/SpringBoot/example7/src/main/java/com/spring/boot/Main.java
@@ -1,8 +1,27 @@
 package com.spring.boot;
 
-public class Main {
-    public static void main(String[] args) {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-        System.out.println("Hello, World!");
+@SpringBootApplication
+@EnableConfigurationProperties(ApplicationConfig.class)
+public class Main implements CommandLineRunner {
+
+    @Autowired
+    private ApplicationConfig config;
+
+    @Autowired
+    private SomeClass someClass;
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println(someClass + " " + config);
     }
 }
