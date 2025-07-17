5e567d2581f42d1e8dd2b40867c7a10206096a12
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-07-17 08:37:30 +0530

    Configuring datasource

--- module-07/SpringBoot/example8/src/main/java/com/spring/boot/Main.java
+++ module-07/SpringBoot/example8/src/main/java/com/spring/boot/Main.java
@@ -1,7 +1,25 @@
 package com.spring.boot;
 
-public class Main {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@SpringBootApplication
+public class Main implements CommandLineRunner {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        SpringApplication.run(Main.class);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        jdbcTemplate.queryForList("select email from employee", String.class)
+                .forEach(System.out::println);
+
     }
 }
