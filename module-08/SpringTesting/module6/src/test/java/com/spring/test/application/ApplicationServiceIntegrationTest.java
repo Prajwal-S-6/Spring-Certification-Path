28cec745e3e102bc27f785a79104e0bad37add5f
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-07-21 08:24:22 +0530

    Spring Test

--- module-08/SpringTesting/module6/src/test/java/com/spring/test/application/ApplicationServiceIntegrationTest.java
+++ module-08/SpringTesting/module6/src/test/java/com/spring/test/application/ApplicationServiceIntegrationTest.java
@@ -6,6 +6,7 @@ import com.spring.test.ds.Guest;
 import com.spring.test.ds.Reservation;
 import com.spring.test.ds.Room;
 import com.spring.test.repository.GuestRepository;
+import com.spring.test.repository.ReservationRepository;
 import com.spring.test.repository.RoomRepository;
 import com.spring.test.service.BookingService;
 import com.spring.test.service.GuestRegistrationService;
@@ -13,6 +14,7 @@ import junit.framework.TestCase;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -22,6 +24,9 @@ import static com.spring.test.ds.BookingResult.BookingState.NO_ROOM_AVAILABLE;
 import static com.spring.test.ds.BookingResult.BookingState.ROOM_BOOKED;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
 
 @RunWith(SpringRunner.class)
 @ContextConfiguration(classes = ApplicationConfiguration.class)
@@ -42,6 +47,9 @@ public class ApplicationServiceIntegrationTest {
     @Autowired
     private GuestRepository guestRepository;
 
+    @Autowired
+    private ReservationRepository reservationRepository;
+
     private static final LocalDate date = LocalDate.of(2025, 7, 20);
 
 
@@ -51,10 +59,12 @@ public class ApplicationServiceIntegrationTest {
 
         assertEquals(ROOM_BOOKED, bookingResult.getBookingState());
         assertThat(bookingResult.getReservation()).isNotEmpty();
+        assertTrue(guestRepository.existsById(1));
     }
 
 
     @Test
+    @DirtiesContext
     public void shouldRejectReservationWhenNoRoom() {
         roomRepository.deleteAll();
 
@@ -62,14 +72,51 @@ public class ApplicationServiceIntegrationTest {
 
         assertEquals(NO_ROOM_AVAILABLE,bookingResult.getBookingState());
         assertThat(bookingResult.getReservation()).isEmpty();
+        assertThat(guestRepository.findAll()).isEmpty();
     }
 
     @Test
+    @DirtiesContext
     public void shouldRegisterGuest() {
         Guest registeredGuest = applicationService.registerGuest("P","S");
 
         assertEquals(new Guest(1, "P", "S"), registeredGuest);
         assertThat(guestRepository.findById(1)).isNotEmpty();
+    }
+
+
+    @Test
+    @DirtiesContext
+    public void shouldBookRoomForRegisteredGuest() {
+        Guest guest = applicationService.registerGuest("P","S");
+
+        BookingResult bookingResult = applicationService.bookAnyRoomForRegisteredGuest(guest, date);
+        assertEquals(ROOM_BOOKED, bookingResult.getBookingState());
+        assertThat(bookingResult.getReservation()).isNotEmpty();
+
+    }
+
+    @Test
+    @DirtiesContext
+    public void shouldBookSpecificRoom() {
+        Guest guest = applicationService.registerGuest("P","S");
+        BookingResult bookingResult = applicationService.bookSpecificRoomForRegisteredGuest(guest, "Yellow Room", date);
+
+        assertEquals(ROOM_BOOKED, bookingResult.getBookingState());
+        assertThat(bookingResult.getReservation()).isNotEmpty();
+        assertEquals("Yellow Room", bookingResult.getReservation().orElseThrow().getRoom().getName());
+        assertEquals(LocalDate.of(2025, 7, 20), bookingResult.getReservation().get().getReservationDate());
+    }
+
+    @Test
+    @DirtiesContext
+    public void shouldRejectWhenNoRoomAvailable() {
+        Guest guest = applicationService.registerGuest("P","S");
+        roomRepository.deleteAll();
+        BookingResult bookingResult = applicationService.bookAnyRoomForRegisteredGuest(guest, date);
+
+        assertEquals(NO_ROOM_AVAILABLE,bookingResult.getBookingState());
+        assertThat(bookingResult.getReservation()).isEmpty();
     }
 
 
