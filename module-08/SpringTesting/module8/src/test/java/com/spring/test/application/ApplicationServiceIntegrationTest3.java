b9ab60fa48dabcb3b7c5621a9a354816767586f6
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-07-22 11:17:37 +0530

    Spring Test - Context share

---
+++ module-08/SpringTesting/module8/src/test/java/com/spring/test/application/ApplicationServiceIntegrationTest3.java
@@ -0,0 +1,62 @@
+package com.spring.test.application;
+
+import com.spring.test.context.approach3.IntegrationTestBase3;
+import com.spring.test.context.aproach1.IntegrationTestBase1;
+import com.spring.test.ds.Guest;
+import com.spring.test.ds.Room;
+import com.spring.test.service.GuestSharableDataService;
+import com.spring.test.service.HotelManagementService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static com.spring.test.configuration.TestDataConfiguration.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringRunner.class)
+@IntegrationTestBase3
+public class ApplicationServiceIntegrationTest3  {
+
+    @Autowired
+    private HotelManagementService hotelManagementService;
+
+    @Autowired
+    private GuestSharableDataService guestSharableDataServiceMock;
+
+    @Autowired
+    private ApplicationService applicationService;
+
+    @Test
+    public void shouldGetListOfRooms() {
+       Set<Room> roomSet = applicationService.listRooms();
+
+       assertThat(roomSet.stream().map(Room::getName).collect(Collectors.toSet())).containsExactlyInAnyOrder(ROOM_A, ROOM_B, ROOM_C);
+
+    }
+
+
+    @Test
+    public void shouldFetchGuestSharableData() {
+        when(guestSharableDataServiceMock.getGuestSharableData()).thenReturn("PS, HS");
+
+        String data = applicationService.getGuestSharableData();
+
+        assertEquals("PS, HS", data);
+    }
+
+    @Test
+    public void shouldGetListOfGuests() {
+        List<Guest> guests = applicationService.listGuests();
+
+        assertThat(guests).containsExactlyInAnyOrder(new Guest(1, "P","S"),
+                new Guest(2, "H","S"),
+                new Guest(3, "G", "K"));
+    }
+}
--- module-08/SpringTesting/module8/src/test/java/com/spring/test/context/approach3/IntegrationTestBase3.java
+++ module-08/SpringTesting/module8/src/test/java/com/spring/test/context/approach3/IntegrationTestBase3.java
@@ -7,7 +7,10 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
 @ContextConfiguration(classes = ApplicationConfiguration.class)
 @ActiveProfiles("test")
 @Sql(value = "/test-data-guests.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_CLASS)
