789317f59ce702e3197cb1ebb34655516c41fd87
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-07-23 08:33:57 +0530

    Spring Test - @Transactional

--- module-08/SpringTesting/module9/src/test/java/com/spring/test/application/ApplicationServiceIntegrationTest.java
+++ module-08/SpringTesting/module9/src/test/java/com/spring/test/application/ApplicationServiceIntegrationTest.java
@@ -2,6 +2,7 @@ package com.spring.test.application;
 
 import com.spring.test.configuration.ApplicationConfiguration;
 import com.spring.test.ds.Guest;
+import com.spring.test.ds.Room;
 import com.spring.test.service.GuestRegistrationService;
 import com.spring.test.service.HotelManagementService;
 import junit.framework.TestCase;
@@ -9,6 +10,8 @@ import org.checkerframework.checker.units.qual.A;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Commit;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,6 +23,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @ContextConfiguration(classes = ApplicationConfiguration.class)
+@Transactional
 public class ApplicationServiceIntegrationTest  {
 
     @Autowired
@@ -33,12 +37,25 @@ public class ApplicationServiceIntegrationTest  {
 
 
     @Test
-    @Transactional
+//    @Commit
     public void shouldRegisterGuest() {
         applicationService.registerGuests(new Guest("P", "S"), new Guest("H", "S"));
 
         assertThat(applicationService.listGuests().stream().map(Guest::getFirstName).collect(Collectors.toSet())).containsAll(Arrays.asList("P","H"));
     }
+
+
+    @Test
+//    @Rollback(value = false)
+    public void shouldRegisterRoom() {
+        applicationService.registerRooms(new Room("A", "A"),new Room("B", "B"));
+
+        assertThat(applicationService.listRooms().stream().map(Room::getName).collect(Collectors.toSet())).containsAll(Arrays.asList("A","B"));
+
+
+    }
+
+
 
 
 
