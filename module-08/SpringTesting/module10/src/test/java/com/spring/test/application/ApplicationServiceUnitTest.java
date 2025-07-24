7f299c003c2ef0733bc2e0548dd95c4205799a6d
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-07-24 08:28:08 +0530

    Spring Test - Mockito

--- module-08/SpringTesting/module10/src/test/java/com/spring/test/application/ApplicationServiceUnitTest.java
+++ module-08/SpringTesting/module10/src/test/java/com/spring/test/application/ApplicationServiceUnitTest.java
@@ -70,4 +70,39 @@ public class ApplicationServiceUnitTest {
 
     }
 
+    @Test
+    public void shouldBookAnyRoomForRegisteredGuest() {
+        Guest guest = new Guest("P","S");
+        Room room = new Room("A","A");
+        when(bookingService.findAvailableRoom(date)).thenReturn(Optional.of(room));
+        when(bookingService.bookRoom(room, guest, date)).thenReturn(Optional.of(new Reservation(room, guest, date)));
+
+        BookingResult bookingResult = applicationService.bookAnyRoomForRegisteredGuest(guest, date);
+
+        verify(bookingService).bookRoom(room, guest, LocalDate.of(2025, 7, 24));
+        assertEquals(ROOM_BOOKED, bookingResult.getBookingState());
+        assertThat(bookingResult.getReservation()).isPresent();
+        assertEquals(date, bookingResult.getReservation().get().getReservationDate());
+        assertEquals(guest.getFirstName(), bookingResult.getReservation().get().getGuest().getFirstName());
+        assertEquals(guest.getLastName(), bookingResult.getReservation().get().getGuest().getLastName());
+        assertThat(bookingResult.getReservation().get().getRoom()).isNotNull();
+    }
+
+    @Test
+    public void shouldSpecificRoomForRegisteredGuest() {
+        Guest guest = new Guest("P", "S");
+        when(bookingService.bookRoom("A", guest , date)).thenReturn(Optional.of(new Reservation(new Room("A", "A"), guest, date)));
+
+        BookingResult bookingResult = applicationService.bookSpecificRoomForRegisteredGuest(guest, "A", date);
+
+        verify(bookingService).bookRoom("A", guest, date);
+        assertEquals(ROOM_BOOKED, bookingResult.getBookingState());
+        assertThat(bookingResult.getReservation()).isPresent();
+        assertEquals("A", bookingResult.getReservation().get().getRoom().getName());
+        assertEquals(guest.getFirstName(), bookingResult.getReservation().get().getGuest().getFirstName());
+        assertEquals(guest.getLastName(), bookingResult.getReservation().get().getGuest().getLastName());
+        assertEquals(date, bookingResult.getReservation().get().getReservationDate());
+    }
+
+
 }
