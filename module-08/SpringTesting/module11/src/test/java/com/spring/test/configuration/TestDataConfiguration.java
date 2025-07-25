693537984b54cd04e5f10827bfabbf5ea83f2418
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-07-25 07:44:59 +0530

    Spring Test - ContextConfiguration

---
+++ module-08/SpringTesting/module11/src/test/java/com/spring/test/configuration/TestDataConfiguration.java
@@ -0,0 +1,25 @@
+package com.spring.test.configuration;
+
+import com.spring.test.ds.Room;
+import com.spring.test.repository.RoomRepository;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TestDataConfiguration {
+
+    @Autowired
+    private RoomRepository roomRepository;
+
+    public static final String ROOM_A = "A";
+    public static final String ROOM_B = "B";
+    public static final String ROOM_C = "C";
+
+    @PostConstruct
+    public void registerRoomsInit() {
+        roomRepository.save(new Room(ROOM_A, "A"));
+        roomRepository.save(new Room(ROOM_B, "A"));
+        roomRepository.save(new Room(ROOM_C, "A"));
+    }
+}
