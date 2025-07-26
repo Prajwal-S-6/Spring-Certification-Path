34288d39208d7eb094a5cd349252fa46b03900a0
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-07-26 07:33:51 +0530

    Spring Test - ContextConfiguration

---
+++ module-08/SpringTesting/module12/src/main/java/com/spring/test/repository/ReservationRepository.java
@@ -0,0 +1,20 @@
+package com.spring.test.repository;
+
+
+import com.spring.test.ds.Reservation;
+import com.spring.test.ds.Room;
+import org.springframework.data.repository.CrudRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+
+public interface ReservationRepository extends CrudRepository<Reservation, UUID> {
+
+    List<Reservation> findAll();
+
+    Set<Reservation> findAllByReservationDate(LocalDate reservationDate);
+
+    boolean existsByRoomAndReservationDate(Room room, LocalDate reservationDate);
+}
