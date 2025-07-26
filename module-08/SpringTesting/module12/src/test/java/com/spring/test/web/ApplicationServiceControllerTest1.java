abf2a554382a26c68c9b188f411632b69ea81388
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-07-26 08:34:00 +0530

    Spring Test - SpringBootTest

---
+++ module-08/SpringTesting/module12/src/test/java/com/spring/test/web/ApplicationServiceControllerTest1.java
@@ -0,0 +1,50 @@
+package com.spring.test.web;
+
+import com.spring.test.application.ApplicationService;
+import com.spring.test.ds.Guest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(ApplicationServiceController.class)
+@AutoConfigureJsonTesters
+class ApplicationServiceControllerTest1 {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockitoBean
+    private ApplicationService applicationService;
+
+    @Autowired
+    private JacksonTester<List<Guest>> listOfGuest;
+
+
+
+    @Test
+    public void shouldGetListOfGuests() throws Exception {
+        when(applicationService.listGuests()).thenReturn(List.of(new Guest(1,"P", "S"), new Guest(2,"H", "S")));
+        List<Guest> guests = listOfGuest.parseObject(mvc.perform(MockMvcRequestBuilders.get("/api/guests")
+                        .contentType("/application/json"))
+                .andExpect(status().isOk())
+                        .andReturn().getResponse().getContentAsString());
+        assertEquals(List.of(new Guest(1,"P", "S"), new Guest(2,"H", "S")), guests);
+    }
+
+
+
+}
