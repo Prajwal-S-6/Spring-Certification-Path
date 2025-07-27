80d82c648acfe7681b377c17e0797a25bef36158
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-07-27 07:25:51 +0530

    Spring Test - SpringBootTest

---
+++ module-08/SpringTesting/module12/src/test/java/com/spring/test/web/ApplicationServiceControllerIntegrationTest.java
@@ -0,0 +1,57 @@
+package com.spring.test.web;
+
+import com.spring.test.ds.Guest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureJsonTesters
+class ApplicationServiceControllerIntegrationTest {
+
+    @Autowired
+    private ApplicationServiceController applicationServiceController;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    JacksonTester<List<Guest>> guestsJson;
+
+    @Autowired
+    JacksonTester<Guest> guestJson;
+
+
+    @Test
+    public void shouldRegisterGuest() throws Exception {
+
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.put("/api/guests").contentType(MediaType.APPLICATION_JSON)
+                .content(guestJson.write(new Guest("P","S")).getJson()))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        Guest guest = guestJson.parseObject(mvcResult.getResponse().getContentAsString());
+
+        assertThat(guest.getId()).isNotNull();
+        assertEquals("P", guest.getFirstName());
+        assertEquals("S", guest.getLastName());
+    }
+
+
+
+}
