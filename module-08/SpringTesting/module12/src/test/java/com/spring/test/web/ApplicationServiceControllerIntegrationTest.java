f87ea07e16e6186c7a4fa5b175aac38e138e3aef
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-07-27 07:53:12 +0530

    Spring Test - SpringBootTest

--- module-08/SpringTesting/module12/src/test/java/com/spring/test/web/ApplicationServiceControllerIntegrationTest.java
+++ module-08/SpringTesting/module12/src/test/java/com/spring/test/web/ApplicationServiceControllerIntegrationTest.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -27,6 +28,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @AutoConfigureJsonTesters
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class ApplicationServiceControllerIntegrationTest {
 
     @Autowired
