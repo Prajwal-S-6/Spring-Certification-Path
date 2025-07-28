bc13e8bf1ed892303871ec36d05a90f6cfd3c779
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-07-28 07:54:56 +0530

    Spring Test - SpringBootTest

--- module-08/SpringTesting/module12/src/test/java/com/spring/test/web/ApplicationServiceControllerIntegrationTest2.java
+++ module-08/SpringTesting/module12/src/test/java/com/spring/test/web/ApplicationServiceControllerIntegrationTest2.java
@@ -1,9 +1,16 @@
 package com.spring.test.web;
 
+import com.spring.test.ds.Guest;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,5 +22,16 @@ class ApplicationServiceControllerIntegrationTest2 {
 
     @Autowired
     private TestRestTemplate testRestTemplate;
+
+    @Test
+    public void shouldRegisterGuest() {
+        String url = String.format("http://localhost:%d",port);
+        Guest guest = new Guest("P", "S");
+        Guest guest1 = new Guest("H", "S");
+        testRestTemplate.put(url+"/api/guests", guest);
+        testRestTemplate.put(url+"/api/guests", guest1);
+
+        testRestTemplate.exchange(url+"/api/guests", HttpMethod.GET, null, new ParameterizedTypeReference<List<Guest>>() {});
+    }
 
 }
