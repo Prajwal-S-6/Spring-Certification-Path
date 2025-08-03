5c031f05e72cf3d3c933961ff0feccb344705974
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-08-03 08:42:17 +0530

    @Lookup

--- module-01/Bean/src/main/java/com/certification/spring/preparation/Runner.java
+++ module-01/Bean/src/main/java/com/certification/spring/preparation/Runner.java
@@ -1,5 +1,7 @@
 package com.certification.spring.preparation;
 
+import com.certification.spring.preparation.beans.PrototypeBean1;
+import com.certification.spring.preparation.beans.SingletonBean1;
 import com.certification.spring.preparation.beans.SpringBean1;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -12,5 +14,18 @@ public class Runner {
 
         SpringBean1 springBean1 = context.getBean(SpringBean1.class);
         springBean1.printDependencies();
+
+        SingletonBean1 singletonBean1 = context.getBean(SingletonBean1.class);
+        System.out.println(singletonBean1);
+        System.out.println(singletonBean1.getPrototypeBean1());
+        System.out.println(singletonBean1.getPrototypeBean1());
+
+        SingletonBean1 singletonBean2 = context.getBean(SingletonBean1.class);
+        System.out.println(singletonBean2);
+        System.out.println(singletonBean2.getPrototypeBean1());
+
+
+
+
     }
 }
