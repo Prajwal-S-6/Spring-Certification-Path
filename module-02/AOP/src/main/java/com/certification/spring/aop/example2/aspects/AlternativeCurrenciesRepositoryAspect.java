bed63cd9f2fa804041ef83c11dca9e0a3522792f
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-09-03 20:16:22 +0530

    Test for AOP

--- module-02/AOP/src/main/java/com/certification/spring/aop/example2/aspects/AlternativeCurrenciesRepositoryAspect.java
+++ module-02/AOP/src/main/java/com/certification/spring/aop/example2/aspects/AlternativeCurrenciesRepositoryAspect.java
@@ -4,32 +4,36 @@ import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 @Aspect
 public class AlternativeCurrenciesRepositoryAspect {
 
+    Logger LOG = LoggerFactory.getLogger(AlternativeCurrenciesRepositoryAspect.class);
+
     @Pointcut("this(com.certification.spring.aop.example2.bls.AlternativeCurrenciesRepository)")
     public void thisAlternativeCurrencyRepositoryPointcut() {}
 
     @Before("thisAlternativeCurrencyRepositoryPointcut()")
     public void beforeThisCurrenciesRepository() {
-        System.out.println("Before - this(AlternativeCurrenciesRepository)");
+        LOG.info("Before - this(AlternativeCurrenciesRepository)");
     }
 
     @Before("target(com.certification.spring.aop.example2.bls.AlternativeCurrenciesRepository)")
     public void beforeTargetCurrenciesRepository() {
-        System.out.println("Before - target(AlternativeCurrenciesRepository)");
+        LOG.info("Before - target(AlternativeCurrenciesRepository)");
     }
 
     @Before("bean(alternate_currencies)")
     public void beforeBeanCurrenciesRepository() {
-        System.out.println("Before - bean(AlternativeCurrenciesRepository)");
+        LOG.info("Before - bean(AlternativeCurrenciesRepository)");
     }
 
     @After("bean(alternate_currencies)")
     public void afterBeanCurrenciesRepository() {
-        System.out.println("After - bean(AlternativeCurrenciesRepository)");
+        LOG.info("After - bean(AlternativeCurrenciesRepository)");
     }
 }
