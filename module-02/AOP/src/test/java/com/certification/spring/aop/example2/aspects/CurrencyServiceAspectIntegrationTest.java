0c2800ce98fb315a82b84e2c12c4c1be763c9d76
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-09-03 21:06:06 +0530

    Test for AOP

---
+++ module-02/AOP/src/test/java/com/certification/spring/aop/example2/aspects/CurrencyServiceAspectIntegrationTest.java
@@ -0,0 +1,47 @@
+package com.certification.spring.aop.example2.aspects;
+
+import com.certification.spring.aop.example2.ApplicationConfig;
+import com.certification.spring.aop.example2.bls.CurrencyService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {ApplicationConfig.class})
+class CurrencyServiceAspectIntegrationTest {
+
+    @Autowired
+    private CurrencyService currencyService;
+
+
+    @Test
+    public void shouldLogForGetExchangeRate() {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        PrintStream originalSystemOut = System.out;
+        System.setOut(new PrintStream(byteArrayOutputStream));
+
+        currencyService.getExchangeRate("USD", "INR");
+
+        System.setOut(originalSystemOut);
+        String logMessage = byteArrayOutputStream.toString();
+
+        assertThat(logMessage).contains("Before - transactionAnnotationPointcut");
+        assertThat(logMessage).contains("Before - blsPackagePointcut");
+        assertThat(logMessage).contains("After - blsPackagePointcut");
+        assertThat(logMessage).contains("After - blsPackageAndInTransactionPointcut");
+        assertThat(logMessage).contains("Before - securedClassPointcut");
+        assertThat(logMessage).contains("Before - getExchangeRateMethodPointcut");
+        assertThat(logMessage).contains("After - getExchangeRateMethodPointcut");
+
+
+
+
+    }
+}
