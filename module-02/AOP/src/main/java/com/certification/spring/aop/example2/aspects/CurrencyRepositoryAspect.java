4a97210055e49f07a59b7608d1cf9f7503ce8d08
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-09-03 20:38:37 +0530

    Test for AOP

--- module-02/AOP/src/main/java/com/certification/spring/aop/example2/aspects/CurrencyRepositoryAspect.java
+++ module-02/AOP/src/main/java/com/certification/spring/aop/example2/aspects/CurrencyRepositoryAspect.java
@@ -4,36 +4,42 @@ import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+
 
 @Component
 @Aspect
 public class CurrencyRepositoryAspect {
+    
+    Logger LOG = LoggerFactory.getLogger(CurrencyRepositoryAspect.class);
 
     @Pointcut("this(com.certification.spring.aop.example2.bls.CurrenciesRepository)")
-    public void thisCurrencyRepositoryPointcut() {
+    private void thisCurrencyRepositoryPointcut() {
 
     }
 
     @Before("thisCurrencyRepositoryPointcut()")
     public void beforeThisCurrencyRepositoryPointcut() {
-        System.out.println("Before - this(CurrenciesRepository)");
+        LOG.info("Before - this(CurrenciesRepository)");
     }
 
     @After("thisCurrencyRepositoryPointcut()")
     public void afterThisCurrencyRepositoryPointcut() {
-        System.out.println("After - this(CurrenciesRepository)");
+        LOG.info("After - this(CurrenciesRepository)");
     }
 
 
     @Before("target(com.certification.spring.aop.example2.bls.CurrenciesRepository)")
     public void beforeTargetCurrencyRepositoryPointcut() {
-        System.out.println("Before - target(CurrenciesRepository)");
+        LOG.info("Before - target(CurrenciesRepository)");
     }
 
     @After("target(com.certification.spring.aop.example2.bls.CurrenciesRepository)")
     public void afterTargetCurrencyRepositoryPointcut() {
-        System.out.println("After - target(CurrenciesRepository)");
+        LOG.info("After - target(CurrenciesRepository)");
     }
 
 
