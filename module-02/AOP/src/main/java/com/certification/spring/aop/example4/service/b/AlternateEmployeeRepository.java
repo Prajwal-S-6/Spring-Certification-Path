c4d327ecb8001e434ecea1c88c7472e636fd38ac
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-09-04 22:37:12 +0530

    Test for AOP

--- module-02/AOP/src/main/java/com/certification/spring/aop/example4/service/b/AlternateEmployeeRepository.java
+++ module-02/AOP/src/main/java/com/certification/spring/aop/example4/service/b/AlternateEmployeeRepository.java
@@ -5,18 +5,18 @@ import com.certification.spring.aop.example4.ds.Employee;
 import org.springframework.stereotype.Component;
 
 @Component
-public /*final*/ class AlternateEmployeeRepository {
-    public Employee findEmployeeById(long id) {
+public class AlternateEmployeeRepository {
+    public final Employee findEmployeeById(long id) {
         return new Employee();
     }
 
-    public void saveEmployee(Employee employee) {
+    public final void saveEmployee(Employee employee) {
     }
 
     public final void deleteEmployee(Employee employee) {
     }
 
-    public void findAndUpdateEmployeeById(long id, Employee employeeToMerge) {
+    public final void findAndUpdateEmployeeById(long id, Employee employeeToMerge) {
         Employee employeeById = findEmployeeById(id);
         saveEmployee(employeeById);
     }
