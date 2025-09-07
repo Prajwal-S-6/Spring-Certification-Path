ddf40ad589ab89ccc6cc1d5888eb933b8efb89f2
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-09-07 09:08:02 +0530

    Test for AOP

--- module-02/AOP/src/main/java/com/certification/spring/aop/example9/beans/TransactionHistoryServiceClient.java
+++ module-02/AOP/src/main/java/com/certification/spring/aop/example9/beans/TransactionHistoryServiceClient.java
@@ -2,10 +2,14 @@ package com.certification.spring.aop.example9.beans;
 
 
 import com.certification.spring.aop.example9.annotations.Sanitize;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TransactionHistoryServiceClient {
+    Logger LOG = LoggerFactory.getLogger(TransactionHistoryServiceClient.class);
+
     public void saveTransactionData(String identifier, @Sanitize String data, @Sanitize String privateKey) {
         System.out.println(
                 String.format(
