4f7a9eb9f39c408160323e42769096920dd08843
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-09-07 09:26:12 +0530

    Test for AOP

--- module-02/AOP/src/test/java/com/certification/spring/aop/example9/aspects/SanitizeAspectIntegrationTest.java
+++ module-02/AOP/src/test/java/com/certification/spring/aop/example9/aspects/SanitizeAspectIntegrationTest.java
@@ -1,24 +1,28 @@
 package com.certification.spring.aop.example9.aspects;
 
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.read.ListAppender;
 import com.certification.spring.aop.example9.ApplicationConfig;
 import com.certification.spring.aop.example9.beans.TransactionHistoryServiceClient;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.event.Level;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.system.CapturedOutput;
-import org.springframework.boot.test.system.OutputCaptureExtension;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
-@ExtendWith({SpringExtension.class, MockitoExtension.class, OutputCaptureExtension.class})
+
+@ExtendWith({SpringExtension.class})
 @ContextConfiguration(classes = ApplicationConfig.class)
 class SanitizeAspectIntegrationTest {
 
