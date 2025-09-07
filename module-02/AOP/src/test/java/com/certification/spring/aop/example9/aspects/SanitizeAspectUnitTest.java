a58954aad8bc0191ef511a641be7121222748669
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-09-07 08:45:29 +0530

    Test for AOP

--- module-02/AOP/src/test/java/com/certification/spring/aop/example9/aspects/SanitizeAspectUnitTest.java
+++ module-02/AOP/src/test/java/com/certification/spring/aop/example9/aspects/SanitizeAspectUnitTest.java
@@ -71,6 +71,21 @@ class SanitizeAspectUnitTest {
         verify(proceedingJoinPoint).proceed(expectedArgsTobeCalledWith);
     }
 
+    @Test
+    public void shouldProceedWithSameArgsForNonSanitizeAnnotated() throws Throwable {
+        ProceedingJoinPoint proceedingJoinPoint = mock(ProceedingJoinPoint.class);
+        MethodSignature signature = mock(MethodSignature.class);
+        when(signature.getMethod()).thenReturn(SanitizeAspectUnitTest.class.getDeclaredMethod("argsNotAnnotatedWithSanitize", String.class, String.class, String.class));
+        when(proceedingJoinPoint.getSignature()).thenReturn(signature);
+        Object[] objects = {"a", "b", "c"};
+        when(proceedingJoinPoint.getArgs()).thenReturn(objects);
+        when(proceedingJoinPoint.proceed(any())).thenReturn("Test");
+
+        assertEquals("Test", sanitizeAspect.around(proceedingJoinPoint));
+        Object[] expectedArgsTobeCalledWith = {"a", "b", "c"};
+        verify(proceedingJoinPoint).proceed(expectedArgsTobeCalledWith);
+    }
+
 
 
     private void argsAnnotatedWithSanitize(String identifier, @Sanitize String data, @Sanitize String privateKey) {}
