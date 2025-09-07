41b70dc801ec06edfafe071461aaf38608d728fa
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-09-07 09:17:02 +0530

    Test for AOP

--- module-02/AOP/src/test/java/com/certification/spring/aop/example9/beans/TransactionHistoryServiceClientUnitTest.java
+++ module-02/AOP/src/test/java/com/certification/spring/aop/example9/beans/TransactionHistoryServiceClientUnitTest.java
@@ -1,28 +1,37 @@
 package com.certification.spring.aop.example9.beans;
 
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.read.ListAppender;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
+
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class TransactionHistoryServiceClientUnitTest {
 
-    @InjectMocks
-    TransactionHistoryServiceClient transactionHistoryServiceClient;
+    TransactionHistoryServiceClient transactionHistoryServiceClient = new TransactionHistoryServiceClient();
 
-    @Mock
-    Logger LOG;
 
     @Test
     void shouldLogTheOutput() {
+        Logger logger = (Logger) LoggerFactory.getLogger(TransactionHistoryServiceClient.class);
+        ListAppender<ILoggingEvent> listAppender = new ListAppender<>();
+        listAppender.start();
+        logger.addAppender(listAppender);
+
         transactionHistoryServiceClient.saveTransactionData("123", "abc", "password");
-        verify(LOG, times(1)).info("Sending data, identifier = [{}], data = [{}], privateKey = [{}]", "123", "abc", "password");
+
+        List<ILoggingEvent> logs = listAppender.list;
+        assertEquals(1, logs.size());
+        assertThat()
     }
 }
