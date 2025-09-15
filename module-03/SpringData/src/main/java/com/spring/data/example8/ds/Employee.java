658f13bc3ab3dbaa69366ecd39fb7281ecd530e9
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-09-15 14:32:03 +0530

    Unit test and integration test with approval testing strategy

--- module-03/SpringData/src/main/java/com/spring/data/example8/ds/Employee.java
+++ module-03/SpringData/src/main/java/com/spring/data/example8/ds/Employee.java
@@ -4,4 +4,16 @@ package com.spring.data.example8.ds;
 import java.sql.Date;
 
 public record Employee(int id, String firstName, String lastName, String email, String phoneNumber, Date hireDate, float salary) {
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", hireDate=" + hireDate +
+                ", salary=" + salary +
+                '}';
+    }
 }
