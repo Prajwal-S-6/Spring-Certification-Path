8154b4d4e1fe3e9f03444e3c38aab701acde539d
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-09-15 14:32:07 +0530

    Unit test and integration test with approval testing strategy

--- module-03/SpringData/src/main/java/com/spring/data/example6/service/EmployeeReportService.java
+++ module-03/SpringData/src/main/java/com/spring/data/example6/service/EmployeeReportService.java
@@ -1,6 +1,8 @@
 package com.spring.data.example6.service;
 ;
+import ch.qos.logback.classic.Logger;
 import com.spring.data.example6.dao.EmployeeDao;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -8,31 +10,33 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class EmployeeReportService {
 
+    Logger logger = (Logger) LoggerFactory.getLogger(EmployeeReportService.class);
+
     @Autowired
     private EmployeeDao employeeDao;
 
     public void printReport() {
-        System.out.println("Employee Report Start");
+        logger.debug("Employee Report Start");
 
-        System.out.println("findEmployeesEmails(): " + employeeDao.findEmployeesCount());
-        System.out.println("findEmployeesEmails(): " + employeeDao.findEmployeesCount());
-        System.out.println("findEmployeesEmails(): " + employeeDao.findEmployeesCount());
-        System.out.println("findEmployeesEmails(): " + employeeDao.findEmployeesCount());
-        System.out.println("findEmployeesEmails(): " + employeeDao.findEmployeesCount());
+        employeeDao.findEmployeesCount();
+        employeeDao.findEmployeesCount();
+        employeeDao.findEmployeesCount();
+        employeeDao.findEmployeesCount();
+        employeeDao.findEmployeesCount();
 
-        System.out.println("Employee Report Stop");
+        logger.debug("Employee Report End");
     }
 
     @Transactional
     public void printReportInTransaction() {
-        System.out.println("Employee Report Start - Transactional");
+        logger.debug("Employee Report Start - Transactional");
 
-        System.out.println("Transactional findEmployeesEmails(): " + employeeDao.findEmployeesCount());
-        System.out.println("Transactional findEmployeesEmails(): " + employeeDao.findEmployeesCount());
-        System.out.println("Transactional findEmployeesEmails(): " + employeeDao.findEmployeesCount());
-        System.out.println("Transactional findEmployeesEmails(): " + employeeDao.findEmployeesCount());
-        System.out.println("Transactional findEmployeesEmails(): " + employeeDao.findEmployeesCount());
+        employeeDao.findEmployeesCount();
+        employeeDao.findEmployeesCount();
+        employeeDao.findEmployeesCount();
+        employeeDao.findEmployeesCount();
+        employeeDao.findEmployeesCount();
 
-        System.out.println("Employee Report Stop - Transactional");
+        logger.debug("Employee Report End - Transactional");
     }
 }
