f6d8ade833ff551024e3101ebf40e1bf6d4ee61c
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-09-16 23:23:47 +0530

    Unit test and integration test with approval testing strategy

--- module-03/SpringData/src/main/java/com/spring/data/example16/configuration/DataSourceConfig.java
+++ module-03/SpringData/src/main/java/com/spring/data/example16/configuration/DataSourceConfig.java
@@ -1,6 +1,8 @@
 package com.spring.data.example16.configuration;
 
+import com.spring.data.example16.service.EmployeeService;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
@@ -11,6 +13,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 import javax.sql.DataSource;
 
 @Configuration
+@ComponentScan(basePackages = "com.spring.data.example16")
 @EnableAspectJAutoProxy
 @EnableTransactionManagement
 public class DataSourceConfig {
