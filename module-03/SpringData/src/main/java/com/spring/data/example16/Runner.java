aa35f846e30989593d49d2629a668db71fc33c04
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-09-16 23:23:50 +0530

    Unit test and integration test with approval testing strategy

--- module-03/SpringData/src/main/java/com/spring/data/example16/Runner.java
+++ module-03/SpringData/src/main/java/com/spring/data/example16/Runner.java
@@ -1,14 +1,15 @@
 package com.spring.data.example16;
 
+import com.spring.data.example16.configuration.DataSourceConfig;
 import com.spring.data.example16.service.EmployeeService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan
+
 public class Runner {
 
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Runner.class);
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DataSourceConfig.class);
         context.registerShutdownHook();
 
         EmployeeService employeeService = context.getBean(EmployeeService.class);
