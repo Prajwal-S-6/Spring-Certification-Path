edc67103cdeb85ddb720f9af2c7ddf516b9c7d63
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-09-17 20:12:37 +0530

    Unit test and integration test with approval testing strategy

--- module-03/SpringData/src/test/java/com/spring/data/example29/service/EmployeeServiceTest.java
+++ module-03/SpringData/src/test/java/com/spring/data/example29/service/EmployeeServiceTest.java
@@ -1,12 +1,21 @@
 package com.spring.data.example29.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import com.spring.data.example29.dao.EmployeeDao;
+import com.spring.data.example29.ds.Employee;
+import org.approvaltests.Approvals;
+import org.json.JSONArray;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -22,10 +31,14 @@ class EmployeeServiceTest {
     private EmployeeDao employeeDao;
 
     @Test
-    void shouldSaveAllEmployees() {
+    void shouldSaveAllEmployees() throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        ObjectWriter writer = objectMapper.writerWithDefaultPrettyPrinter();
         employeeService.saveAllEmployees();
 
-        assertThat(employeeDao.findAll().size()).isEqualTo(5);
+        List<Employee> employeeList = employeeDao.findAll();
+        assertThat(employeeList.size()).isEqualTo(5);
+        Approvals.verify(writer.writeValueAsString(employeeList));
     }
 
 }
