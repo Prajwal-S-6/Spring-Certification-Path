532bb6826ec178b325543f8e67e02aa90e33ee46
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-09-17 19:27:15 +0530

    Unit test and integration test with approval testing strategy

---
+++ module-03/SpringData/src/test/java/com/spring/data/example18/service/EmployeeServiceTest.java
@@ -0,0 +1,36 @@
+package com.spring.data.example18.service;
+
+import com.spring.data.example18.configuration.DataSourceConfig;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.system.CapturedOutput;
+import org.springframework.boot.test.system.OutputCaptureExtension;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith({OutputCaptureExtension.class, SpringExtension.class})
+@ContextConfiguration(classes = DataSourceConfig.class)
+class EmployeeServiceTest {
+
+    @Autowired
+    //@Qualifier("employee_service")
+    private EmployeeService employeeService;
+
+    @Test
+    public void shouldCauseRollbackForUncheckedException(CapturedOutput capturedOutput) {
+        try {
+            employeeService.methodWithUncheckedExceptionCausingRollback();
+        } catch (Exception e) {
+
+        }
+        finally {
+            assertThat(capturedOutput.getOut()).contains("rollback");
+        }
+    }
+
+}
