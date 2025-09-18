04ff39907abc7b910644d2e59d7b189ee7143f6b
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-09-18 10:37:41 +0530

    Unit test and integration test with approval testing strategy cucumber testing

---
+++ module-03/SpringData/src/test/java/com/spring/data/example29/service/SpringTestConfig.java
@@ -0,0 +1,12 @@
+//package com.spring.data.example29.service;
+//
+//import io.cucumber.spring.CucumberContextConfiguration;
+//import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.context.annotation.Configuration;
+//
+//@SpringBootTest
+//@CucumberContextConfiguration
+//public class SpringTestConfig {
+//}
